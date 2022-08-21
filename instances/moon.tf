fe8fa20492df7d2b67490f7bf460e5d596927c73
Elizabeth Martín Campos <me@elizabeth.sh>
2022-08-21 10:00:10 +0000

    downgrade moon-1 machine

--- instances/moon.tf
+++ instances/moon.tf
@@ -34,9 +34,23 @@ data "aws_subnet" "moon-1" {
   }
 }
 
+data "aws_security_group" "default-sg" {
+  vpc_id = data.aws_subnet.moon-1.vpc_id
+  name = "default"
+}
+
+data "aws_security_group" "http-sg" {
+  vpc_id = data.aws_subnet.moon-1.vpc_id
+  name = "allow-http"
+}
+
 resource "aws_network_interface" "moon-1_0" {
   subnet_id       = data.aws_subnet.moon-1.id
   private_ips     = ["172.31.48.4"]
+  security_groups = [
+    data.aws_security_group.default-sg.id,
+    data.aws_security_group.http-sg.id
+  ]
 
   tags = {
     Name = "moon-1_0"
@@ -45,7 +59,7 @@ resource "aws_network_interface" "moon-1_0" {
 
 resource "aws_instance" "moon-1" {
   ami           = data.aws_ami.AL2022.id
-  instance_type = "t4g.small"
+  instance_type = "t4g.micro"
   key_name = "me@elizabeth.sh"
 
   tags = {
--- root.tf
+++ root.tf
@@ -1,25 +1,3 @@
 module "instances" {
   source = "./instances"
 }
-
-variable "sinope_ask_server" {
-  type = string
-}
-
-variable "sinope_ask_server_privkey" {
-  type = string
-  sensitive = true
-}
-
-variable "sinope_ask_mongodb_uri" {
-  type = string
-  sensitive = true
-}
-
-module "sinope_ask" {
-  source = "./deployments/sinope_ask"
-
-  host = var.sinope_ask_server
-  host_privkey = var.sinope_ask_server_privkey
-  mongodb_uri = var.sinope_ask_mongodb_uri
-}
